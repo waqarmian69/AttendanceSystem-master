29b54d8a22f6c30fbeb1239f225d79840f037b74
waqarmian69 <waqarmian69@gmail.com>
2021-01-08 22:50:24 -0800

    Sign Up / Sign In Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,32 @@
-import 'package:attendance/screens/LoginScreen.dart';
-import 'package:attendance/screens/MainWidget.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_villains/villains/villains.dart';
+import 'signIn.dart';
 
-import 'library/Authentication.dart';
-import 'library/constant.dart';
+// MIT License
+// Copyright (c) 2020 Sujan Bhattarai
 
-void main() async {
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -14,28 +34,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      navigatorObservers: [new VillainTransitionObserver()],
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        backgroundColor: white,
-        primarySwatch: Colors.blue,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
-      routes: {
-        '/login': (context) => LoginScreen(),
-      },
-      home: FutureBuilder<bool>(
-          future: checkIfAuthenticated(),
-          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-            if (snapshot.hasData) {
-              bool user = snapshot.data;
-              return user ? MainWidget() : LoginScreen();
-            }
-            return CircularProgressIndicator();
-          }),
+      home: SignIn(),
     );
   }
 }
